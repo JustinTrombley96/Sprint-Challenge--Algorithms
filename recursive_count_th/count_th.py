0d82dae5294f039b1950cdf797bb325e545b534e
Justin Trombley <jtrombley96@gmail.com>
2020-05-22 14:49:29 -0400

    Stretch recursive count

--- recursive_count_th/count_th.py
+++ recursive_count_th/count_th.py
@@ -8,19 +8,27 @@ Your function must utilize recursion. It cannot contain any loops.
 def count_th(word):
     target = 'th'
     l = len(word)
-    
     if word == '':
         return 0
 
+
     #Attempt 1 (Fails 2 Tests)
-    # if word[0] + word[1] == target:
-    #     return 1 + count_th(word[2:])
-    # return count_th(word[1:])
+    # Multiple and single
+    if len(word) < 2:
+        return 0
+    elif word[0] + word[1] == target:
+        return 1 + count_th(word[2:])
+    
+    return count_th(word[1:])
 
     #Attempt 2 (Fails 2 Tests)
+    # Backwards and multiple
     # return 'th' in word
 
-
-
+    #Attempt 3 (Fails 2 Tests)
+    # Backwards and multiple
+    # if word[0] + word[1] == target:
+    #     return 1 
+    # return count_th(word[1:])
 
 
